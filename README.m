6fd3c01446b0c7f6537a986692b5d7ffe1726a1e
Delaram Poudel <97503594+Delarram@users.noreply.github.com>
2023-10-17 00:05:44 +0630

    Create README.md

---
+++ README.m
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+  body {
+    font-family: Arial, sans-serif;
+    background-color: #f2f2f2;
+    margin: 0;
+    padding: 0;
+  }
+  h1, h3 {
+    text-align: center;
+  }
+  .container {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 20px;
+    background-color: #fff;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  }
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+  .stats {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 20px;
+  }
+</style>
+</head>
+<body>
+<div class="container">
+  <h1>Hi 👋, I'm Delaram</h1>
+  <h3>Cross-Platform App Developer | Flutter Enthusiast</h3>
+  <img align="right" alt="coding" src="https://i0.wp.com/codemyui.com/wp-content/uploads/2017/03/hero-section-animation.gif?fit=880%2C440&ssl=1">
+  <div class="stats">
+    <p>🔭 I’m currently working at <strong>Digital-dots</strong></p>
+    <p>🌱 I’m currently learning <strong>UI/UX design principles</strong></p>
+    <p>💬 Ask me about <strong>Dart, Flutter, Flutter Web</strong></p>
+    <p>📫 How to reach me <strong>blackpoudel@gmail.com</strong></p>
+  </div>
+  <h3>Connect with me:</h3>
+  <p>
+    <a href="https://linkedin.com/in/delarram" target="_blank"><img src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="delarram" height="30" width="40" /></a>
+    <a href="https://instagram.com/delu" target="_blank"><img src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/instagram.svg" alt="delu" height="30" width="40" /></a>
+  </p>
+  <h3>Languages and Tools:</h3>
+  <p>
+    <a href="https://dart.dev" target="_blank" rel="noreferrer"><img src="https://www.vectorlogo.zone/logos/dartlang/dartlang-icon.svg" alt="dart" width="40" height="40" /></a>
+    <a href="https://www.figma.com/" target="_blank" rel="noreferrer"><img src="https://www.vectorlogo.zone/logos/figma/figma-icon.svg" alt="figma" width="40" height="40" /></a>
+    <a href="https://flutter.dev" target="_blank" rel="noreferrer"><img src="https://www.vectorlogo.zone/logos/flutterio/flutterio-icon.svg" alt="flutter" width="40" height="40" /></a>
+    <a href="https://www.mysql.com/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="mysql" width="40" height="40" /></a>
+  </p>
+</div>
+</body>
+</html>
